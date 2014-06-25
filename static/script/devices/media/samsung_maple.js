941bec1bd30156412232cb1d8d328604940c0d75
jayne-rutherford <jayne_rutherford@hotmail.com>
2014-06-25 09:13:05 +0100

    TVPJSFRMWK-1862 fixed typo in require block.

--- static/script/devices/media/samsung_maple.js
+++ static/script/devices/media/samsung_maple.js
@@ -28,7 +28,7 @@ require.def(
     'antie/devices/media/samsung_maple',
     [
         'antie/devices/device',
-        'antie/devices/media//mediacontroller',
+        'antie/devices/media/mediacontroller',
         'antie/events/mediaevent',
         'antie/events/mediaerrorevent',
         'antie/events/mediasourceerrorevent',
